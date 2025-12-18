2298cf6571bab23a059974fcefe829f41a1f5b49
jinguihua <156711203@qq.com>
2025-12-18 14:25:57 +0800

    changed

--- cache.kv.go
+++ cache.kv.go
@@ -1,13 +1,15 @@
 package myredis
 
+import "fmt"
+
 // KVCache 只负责通用缓存：Get / Set / Del
-type KVCache[T any] struct {
+type KVCache[K comparable, V any] struct {
 	client    Client
 	namespace string // 业务前缀
 	exp       int
 }
 
-func NewKVCache[T any](c Client, namespace string, ttlSec int) *KVCache[T] {
+func NewKVCache[K comparable, V any](c Client, namespace string, ttlSec int) *KVCache[K, V] {
 	if c == nil {
 		panic("client is nil")
 	}
@@ -16,30 +18,30 @@ func NewKVCache[T any](c Client, namespace string, ttlSec int) *KVCache[T] {
 		panic("key is empty")
 	}
 
-	return &KVCache[T]{client: c, namespace: namespace, exp: ttlSec}
+	return &KVCache[K, V]{client: c, namespace: namespace, exp: ttlSec}
 }
 
 // 拼接最终存储 key
-func (c *KVCache[T]) buildKey(key string) string {
-	return c.namespace + "::" + key
+func (c *KVCache[K, V]) buildKey(key K) string {
+	return fmt.Sprintf("%s::%v", c.namespace, key)
 }
 
-func (c *KVCache[T]) Set(key string, value T) error {
+func (c *KVCache[K, V]) Set(key K, value V) error {
 	ttl := c.exp
 	return c.client.Set(c.buildKey(key), value, ttl)
 }
 
-func (c *KVCache[T]) Get(key string) (T, error) {
-	var ret T
+func (c *KVCache[K, V]) Get(key K) (V, error) {
+	var ret V
 	err := c.client.Get(c.buildKey(key), &ret)
 	return ret, err
 }
 
-func (c *KVCache[T]) Del(key string) error {
+func (c *KVCache[K, V]) Del(key K) error {
 	return c.client.Del(c.buildKey(key))
 }
 
-func (c *KVCache[T]) Expire(key string, exp ...int) error {
+func (c *KVCache[K, V]) Expire(key K, exp ...int) error {
 	ttl := c.exp
 	if len(exp) > 0 {
 		ttl = exp[0]
